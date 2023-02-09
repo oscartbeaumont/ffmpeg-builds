ea4014d5661feac5933d7653fa6239d60a2e136f
Oscar Beaumont <oscar@otbeaumont.me>
2023-02-09 15:09:48 +0800

    give it the sauce

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Install dependencies for build"
+    cargo install cargo-c
+
+    echo "Clone"
+    git clone https://github.com/xiph/rav1e.git
+    cd rav1e/
+
+    echo "Checkout"
+    git checkout v0.6.3
+
+    echo "Build"
+    cargo build --release
+
+    echo "Cinstall"
+    cargo cinstall --release
+
+    cd ../
+elif [[ $OSTYPE == 'darwin'* ]]; then
+    # Brew has the wrong version so we build from scratch
+    echo "Clone"
+    git clone https://github.com/Haivision/srt.git
+    cd srt/
+
+    echo "Configure"
+    export OPENSSL_ROOT_DIR=$(brew --prefix openssl)
+    export OPENSSL_LIB_DIR=$(brew --prefix openssl)"/lib"
+    export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)"/include"
+    ./configure
+
+    echo "Build"
+    make
+
+    cd ../
+fi
